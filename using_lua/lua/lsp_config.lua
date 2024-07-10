abe4110d99b3c0b93abdbe1d402f181b4d2fe4d6
TRAN NGOC QUANG <quang.tran.ngoc@aiactiv.io>
2024-07-10 17:10:18 +0700

    add: lsp python + fold

--- using_lua/lua/lsp_config.lua
+++ using_lua/lua/lsp_config.lua
@@ -1,2 +1,11 @@
 local nvim_lsp = require('lspconfig')
-nvim_lsp.gopls.setup {}
+--nvim_lsp.gopls.setup {}
+local servers = {
+  "gopls",
+  "pyright",
+  "tsserver",
+}
+
+for _, server in pairs(servers) do
+  nvim_lsp[server].setup{}
+end
--- using_lua/lua/plugins/init.lua
+++ using_lua/lua/plugins/init.lua
@@ -189,5 +189,60 @@ return {
          --
           })
     end
+  },
+  {
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  opts = {
+    -- add any options here
+  },
+  dependencies = {
+    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+    "MunifTanjim/nui.nvim",
+    -- OPTIONAL:
+    --   `nvim-notify` is only needed, if you want to use the notification view.
+    --   If not available, we use `mini` as the fallback
+    "rcarriga/nvim-notify",
+    }
+  },
+  {
+    'folke/noice.nvim',
+    config = function ()
+      require('noice').setup({
+        lsp = {
+          -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+          override = {
+            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+            ["vim.lsp.util.stylize_markdown"] = true,
+            ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+          },
+        },
+        -- you can enable a preset for easier configuration
+        presets = {
+          bottom_search = true, -- use a classic bottom cmdline for search
+          command_palette = true, -- position the cmdline and popupmenu together
+          long_message_to_split = true, -- long messages will be sent to a split
+          inc_rename = false, -- enables an input dialog for inc-rename.nvim
+          lsp_doc_border = false, -- add a border to hover docs and signature help
+        },
+      })
+    end
+  },
+  {
+    'linux-cultist/venv-selector.nvim',
+    dependencies = { 'neovim/nvim-lspconfig', 'nvim-telescope/telescope.nvim', 'mfussenegger/nvim-dap-python' },
+    config = function()
+      require('venv-selector').setup {
+        -- Your options go here
+        -- name = "venv",
+        -- auto_refresh = false
+    }
+    end,
+    keys = {
+      -- Keymap to open VenvSelector to pick a venv.
+      { '<leader>vs', '<cmd>VenvSelect<cr>' },
+      -- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
+      { '<leader>vc', '<cmd>VenvSelectCached<cr>' },
+    },
   }
 }
